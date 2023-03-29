532fe4e83b00457fc1f12ebd707e825d694781b4
Rom Orlovich <romorlovich@gmail.com>
2023-03-29 15:25:06 +0300

    initlize react.ts-taliwnds app

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="w-[100vw] h-[100vh] bg-blue-500">
+      <header className="">
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
