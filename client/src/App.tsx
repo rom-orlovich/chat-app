c22b81704985afb3058a214ffc38fd224666f311
Rom Orlovich <romorlovich@gmail.com>
2023-03-29 16:41:59 +0300

    initlize docker compose for client and server service

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-
-
-
+import React from "react";
 function App() {
   return (
-    <div className="w-[100vw] h-[100vh] bg-blue-500">
+    <div className="w-[100vw] h-[100vh] bg-white-500">
       <header className="">
-    
+      
       </header>
     </div>
   );
