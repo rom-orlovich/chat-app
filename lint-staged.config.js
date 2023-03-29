dbbfce9a223a6600088797ce268ed64a68195987
Rom Orlovich <romorlovich@gmail.com>
2023-03-30 01:17:16 +0300

    create lint-staged config

---
+++ lint-staged.config.js
@@ -0,0 +1,12 @@
+// Lint-staged.config.js
+module.exports = {
+  // Type check TypeScript files
+  "**/*.(ts|tsx)": () => "npx tsc --noEmit",
+
+  // Lint then format TypeScript and JavaScript files
+  "**/*.(ts|tsx|js)": (filenames) => [`eslint --fix ${filenames.join(" ")}`],
+
+  // Format MarkDown and JSON
+  "**/*.(md|json)": (filenames) =>
+    `npx prettier --write ${filenames.join(" ")}`,
+};
