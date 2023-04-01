7d972d2e552ea2c56beeba258c88b165f4d8a714
Rom Orlovich <romorlovich@gmail.com>
2023-04-01 20:39:33 +0300

    finish refactor the server

--- server/src/mongoDB/utils.ts
+++ server/src/mongoDB/utils.ts
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb";
 
 export const client = new MongoClient(process.env.MONGO_DB_URI || "");
-
+/**
+ *  Get collection from mongoDB.
+ */
 export const getCollection = (collName: string, dbName = "chat-app") =>
   client.db(dbName).collection(collName);
